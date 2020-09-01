95b7687192e0120429f54888e8c3581d77cae037
Dennis von Bargen <dennis.vonbargen@soprasteria.com>
2020-09-01 19:06:52 +0200

    Home text separated

--- PortfolioReact.Web/ClientApp/src/components/Home.tsx
+++ PortfolioReact.Web/ClientApp/src/components/Home.tsx
@@ -5,7 +5,9 @@ export const Home: React.FunctionComponent = () => {
     return (
         <div className="Home-component">
             <img src={ProfilePicture} className="border border-5 border-white rounded-circle mx-auto d-block" alt="Profile" />
-            <h1 className="home-text text-white text-center">Full-stack developer, Pilot, Problem solver</h1>
+            <h1 className="home-text text-white text-center">Full-stack developer</h1>
+            <h1 className="home-text text-white text-center">Pilot</h1>
+            <h1 className="home-text text-white text-center">Problem solver</h1>
         </div>
     );
 }
