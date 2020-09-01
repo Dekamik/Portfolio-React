fef088c3bc349f75873d199124329981f68b5fd7
Dennis von Bargen <dennis.vonbargen@soprasteria.com>
2020-09-01 19:17:49 +0200

    Moved home picture down

--- PortfolioReact.Web/ClientApp/src/components/Home.tsx
+++ PortfolioReact.Web/ClientApp/src/components/Home.tsx
@@ -4,10 +4,11 @@ import ProfilePicture from './../images/jag.jpg';
 export const Home: React.FunctionComponent = () => {
     return (
         <div className="Home-component">
-            <img src={ProfilePicture} className="border border-5 border-white rounded-circle mx-auto d-block" alt="Profile" />
             <h1 className="home-text text-white text-center">Full-stack developer</h1>
             <h1 className="home-text text-white text-center">Pilot</h1>
             <h1 className="home-text text-white text-center">Problem solver</h1>
+            <br/>
+            <img src={ProfilePicture} className="border border-5 border-white rounded-circle mx-auto d-block" alt="Profile" />
         </div>
     );
 }
