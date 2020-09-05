9bb3e3e7461f3a1f8553e543e1b26df0dcd91caa
Dennis von Bargen <dennis@vonbargen.se>
2020-09-05 13:10:13 +0200

    More repsonsive home page

--- PortfolioReact.Web/ClientApp/src/components/Home.tsx
+++ PortfolioReact.Web/ClientApp/src/components/Home.tsx
@@ -5,10 +5,12 @@ export const Home: React.FunctionComponent = () => {
     return (
         <div className="Home-component">
             <h1 className="home-text text-white text-center">Full-stack developer</h1>
+            <h1 className="home-text text-white text-center">Consultant</h1>
             <h1 className="home-text text-white text-center">Pilot</h1>
-            <h1 className="home-text text-white text-center">Problem solver</h1>
-            <br/>
-            <img src={ProfilePicture} className="border border-5 border-white rounded-circle mx-auto d-block" alt="Profile" />
+            <br />
+            <div className="margin-15">
+                <img src={ProfilePicture} className="border border-5 border-white rounded-circle mx-auto d-block img-fluid" alt="Profile" />
+            </div>
         </div>
     );
 }
