5fd02e3d1d9ce52db1a6a0bf657772402b3ca87d
Dennis von Bargen <dennis@vonbargen.se>
2020-09-05 15:18:20 +0200

    Skeleton for AboutMe page and scroll fix

--- PortfolioReact.Web/ClientApp/src/components/AboutMe.tsx
+++ PortfolioReact.Web/ClientApp/src/components/AboutMe.tsx
@@ -3,7 +3,18 @@
 export const AboutMe: React.FunctionComponent = () => {
     return (
         <>
-            <h1>About me</h1>
+            <h2>Who am I?</h2>
+            <p>
+
+            </p>
+            <h2>What can I deliver?</h2>
+            <p>
+
+            </p>
+            <h2>What motivates me?</h2>
+            <p>
+
+            </p>
         </>
     );
 }
