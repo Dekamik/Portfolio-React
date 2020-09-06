6e5a84ec9dc6c3bcdbd0afb5606e6096b0e229be
Dennis von Bargen <dennis@vonbargen.se>
2020-09-06 17:53:01 +0200

    Cleaned up Startup

--- PortfolioReact.Api/Startup.cs
+++ PortfolioReact.Api/Startup.cs
@@ -1,21 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace PortfolioReact.Api
 {
     public class Startup
     {
-        private string _corsPolicy = "AllowAll";
+        private readonly string _corsPolicy = "AllowAll";
 
         public Startup(IConfiguration configuration)
         {
