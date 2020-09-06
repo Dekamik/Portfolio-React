37c7f7f1ada37cfa808b51b9c9b1dd144f3652de
Dennis von Bargen <dennis@vonbargen.se>
2020-09-06 11:13:33 +0200

    Loading spinner created and added to Projects page

--- PortfolioReact.Web/ClientApp/src/components/projects/Projects.tsx
+++ PortfolioReact.Web/ClientApp/src/components/projects/Projects.tsx
@@ -2,26 +2,36 @@
 import { IProject } from './IProject';
 import { ProjectCard } from './ProjectCard';
 import { ProjectsApi } from './ProjectsApi';
+import { Spinner } from './../common/Spinner';
 
 export const Projects: React.FunctionComponent = () => {
 
     const api = new ProjectsApi();
 
     const [projects, setProjects] = React.useState<IProject[]>([]);
+    const [isLoading, setIsLoading] = React.useState<boolean>(true);
 
     React.useEffect(() => {
-        api.getProjects((data: IProject[]) => {
-            setProjects(data);
-        });
+        api.getProjects(
+            (data: IProject[]) => {
+                setProjects(data);
+            },
+            (message: string) => {
+
+            },
+            () => {
+                setIsLoading(false);
+            }
+        );
     }, [api]);
 
     return (
-        <>
+        <Spinner isLoading={isLoading}>
             {
                 projects.map((project, i) =>
                     <ProjectCard key={i} project={project}/>
                 )
             }
-        </>
+        </Spinner>
     );
 }
